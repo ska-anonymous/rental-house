ce6d465bcc6481349425897a0ffa850e17047829
Salman Khan A <salmankhanserai@gmail.com>
2024-08-02 16:09:22 +0500

    Slight changes in the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,13 +48,6 @@ const Contact = () => {
             </div>
             <div className="container-fluid">
                 <div className="row justify-content-center">
-                    {/* <div className="col-md-6 px-0">
-                        <div className="map_container">
-                            <div className="map-responsive">
-                                <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameBorder="0" style={{ border: 0, width: "100%", height: "100%" }} allowFullScreen></iframe>
-                            </div>
-                        </div>
-                    </div> */}
                     <div className="col-lg-4 col-md-5 ">
                         <div className="form_container">
                             <form onSubmit={handleFormSubmit}>
